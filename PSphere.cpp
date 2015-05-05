9fa0925b7b578d58154b34f76ba1d14732f4e245
Giovanni <giogix2@gmail.com>
2015-05-05 12:20:00 +0300

    Fixed attachMesh() and modified textures
    Debugged attachMesh().
    Changed the content of the material files and changed the format of the
    textures.

--- PSphere.cpp
+++ PSphere.cpp
@@ -583,10 +583,10 @@ void PSphere::loadMeshFile(const std::string &path, const std::string &meshName)
 
 void PSphere::attachMesh(Ogre::SceneNode *node, Ogre::SceneManager *scene, const std::string &objectName, Ogre::Real x, Ogre::Real y, Ogre::Real z) {
 	Ogre::Vector3 position = Ogre::Vector3(x, y, z);
-	Object object = Object(position, objectName);	
-	//objects.push_back(object);
 	Ogre::Entity *entity = scene->createEntity(objectName, objectName);
-	Ogre::SceneNode *cube = node->createChildSceneNode(objectName, Ogre::Vector3(x, y, z));
+	Ogre::SceneNode *cube = node->createChildSceneNode(objectName, position);
+	Object object = Object(position, objectName, node);	
+	objects.push_back(object);
 	cube->attachObject(entity);
 }
 
