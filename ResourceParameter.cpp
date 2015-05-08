bbbdb998f6179d9038f1777a4e2de5b9540231ba
Taneli Mikkonen <Lauri.Mikkonen@student.oulu.fi>
2015-05-08 18:10:20 +0300

    Replace atof with istringstream
    On my Linux box atof (in ResourceParameter.cpp's
    splitToFrequencyAmplitude()) returned zero on perfectly good strings,
    resulting to invisible planet. Changed to use istringstream instead.

--- ResourceParameter.cpp
+++ ResourceParameter.cpp
@@ -233,8 +233,12 @@ namespace std
 		{
 			stringTemp2 = stringToSplit.substr(i, stringToSplit.length());
 		}
-		frequency = std::atof(stringTemp1.c_str());
-		amplitude = std::atof(stringTemp2.c_str());
+		/* std::atof(string.c_str()) returned zero with perfectly good string.
+		 * Using istringstream instead. */
+		istringstream convStream1(stringTemp1);
+		convStream1 >> frequency;
+		istringstream convStream2(stringTemp2);
+		convStream2 >> amplitude;
 
 		//control the range
 		frequency = frequency < 0 ? 0 : frequency;
