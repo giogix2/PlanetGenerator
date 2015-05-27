7b42e0b6c6b88df090cb30038e6a0db706732c2d
Taneli Mikkonen <Lauri.Mikkonen@student.oulu.fi>
2015-05-27 23:24:56 +0300

    Windows specific headers were not inside ifdefs

--- main.cpp
+++ main.cpp
@@ -32,14 +32,14 @@
 #include "testui2/mainwindow.h"
 #include <QApplication>
 #include "ResourceParameter.h"
-#include <io.h>
-#include <fcntl.h>
 
 
 using namespace std;
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
+#include <io.h>
+#include <fcntl.h>
 #include "windows.h"
 #endif
 
